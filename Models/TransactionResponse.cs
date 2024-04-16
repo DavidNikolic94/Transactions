ca0dda663497fe07017f2e212652f5869900be7f
David Nikolic <Davidnikolic94@gmail.com>
2024-04-16 22:42:32 +0200

    Add: TransactionResponse model

---
+++ Models/TransactionResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Transactions.Models
+{
+
+    public class TransactionResponse
+    {
+        public string? TransactionId { get; set; }
+        public string? Message { get; set; }
+        public int Status { get; set; }
+    }
+}
