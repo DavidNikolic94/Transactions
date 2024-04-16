ec03d716b87c87b9b219a4658e05b09dacc02ccd
David Nikolic <Davidnikolic94@gmail.com>
2024-04-16 22:40:47 +0200

    Add: TransactionRequest model

---
+++ Models/TransactionRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Transactions.Models
+{
+    public class TransactionRequest
+    {
+        public string? ExternalTransactionId { get; set; }
+        public string? UserId { get; set; }
+        public decimal Amount { get; set; }
+        public string? Currency { get; set; }
+    }
+
+}
