02ec825940155b7b18c938eaa2751f2e81f8622e
David Nikolic <Davidnikolic94@gmail.com>
2024-04-16 23:44:33 +0200

    Add: Users model

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+﻿namespace Transactions.Models
+{
+    public class User
+    {
+        public string? Id { get; set; }
+        public decimal Balance { get; set; }
+
+        public string? UserName { get; set; }
+        public string? Currency { get; set; }
+    }
+
+}
