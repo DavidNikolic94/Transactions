895c890d48e5a5e65514f5ff3ce830f6ce653004
David Nikolic <Davidnikolic94@gmail.com>
2024-04-16 22:39:02 +0200

    Add: Folder 'Models' and Transaction model

---
+++ Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Transactions.Models
+{
+    public class Transaction
+    {
+        public string? TransactionId { get; set; }
+        public string? ExternalTransactionId { get; set; }
+        public string? UserId { get; set; }
+        public decimal Amount { get; set; }
+        public string? Currency { get; set; }
+        public string? Message { get; set; }
+        public int Status { get; set; }
+        public DateTime Timestamp { get; set; }
+        public string? Hash { get; set; }
+    }
+}
