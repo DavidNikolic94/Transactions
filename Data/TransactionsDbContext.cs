f0507badf9ca5b5878977e22a018d1cc858581e0
David Nikolic <Davidnikolic94@gmail.com>
2024-04-17 16:54:04 +0200

    Add: Context class for the database

---
+++ Data/TransactionsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+using Transactions.Models;
+
+namespace Transactions.Data
+{
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        {
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("Users");
+
+            modelBuilder.Entity<User>().Property(u => u.Balance).HasPrecision(18, 4);
+        }
+
+    }
+}
