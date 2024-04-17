4c2d93f710eb87667f466c7444cd39c29164bc8c
David Nikolic <Davidnikolic94@gmail.com>
2024-04-17 20:15:07 +0200

    Mod: appsetting.json and Programs.cs added the connection string

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Transactions.Data;
+using Transactions.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +10,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Transactions", Version = "v1" });
+
+});
+
+
+// Add services to the container.
+builder.Services.AddDbContext<TransactionDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add services to the container.
+builder.Services.AddScoped<ITransactionService, TransactionService>();
 
 var app = builder.Build();
 
