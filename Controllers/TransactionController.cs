c4053dd2b6a4785d8678f1741a013085b7e8fa80
David Nikolic <Davidnikolic94@gmail.com>
2024-04-17 17:08:15 +0200

    Add: Basic controller for returning the status of the request

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Transactions.Models;
+using Transactions.Services;
+
+[ApiController]
+[Route("[controller]")]
+public class TransactionController : ControllerBase
+{
+    private readonly ITransactionService _transactionService;
+    private readonly ILogger<TransactionController> _logger;
+
+    public TransactionController(ITransactionService transactionService, ILogger<TransactionController> logger)
+    {
+        _transactionService = transactionService;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Post([FromBody] TransactionRequest request, string fromUserId)
+    {
+        try
+        {
+
+            var response = await _transactionService.ProcessTransaction(request, fromUserId);
+
+            return Ok(response);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "An error occurred while processing your request.");
+        }
+    }
+}
