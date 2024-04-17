ae42c03d69cd597a8374d470c24605c59690d8f7
David Nikolic <Davidnikolic94@gmail.com>
2024-04-17 20:52:51 +0200

    Mod: SecretKeyGenerator/Program.cs modified to write the secret key in the `EnvironmentVariableTarget`

---
+++ SecretKeyGenerator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var secretKey = Environment.GetEnvironmentVariable("SECRET_KEY");
+        Console.WriteLine(secretKey);
+        GenerateAndStoreSecretKey();
+    }
+
+    private static void GenerateAndStoreSecretKey()
+    {
+        string secretKey = SecretKeyGenerator.GenerateSecretKey();
+        Console.WriteLine($"Generated Secret Key: {secretKey}");
+
+        Environment.SetEnvironmentVariable("SECRET_KEY", secretKey, EnvironmentVariableTarget.User);
+    }
+}
+
+public class SecretKeyGenerator
+{
+    public static string GenerateSecretKey()
+    {
+        using (var rng = new RNGCryptoServiceProvider())
+        {
+            byte[] secretKey = new byte[32];
+            rng.GetBytes(secretKey);
+            return Convert.ToBase64String(secretKey);
+        }
+    }
+}
+
