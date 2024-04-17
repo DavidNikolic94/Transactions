d427456f8895edb39e35b6c4412ee17584b916fa
David Nikolic <Davidnikolic94@gmail.com>
2024-04-17 20:31:07 +0200

    Add: AddHashHeaderParameter class for adding the header parameter in SwaggerUI

---
+++ Services/AddHashHeaderParameter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Transactions.Services
+{
+    public class AddHashdHeaderParameter : IOperationFilter
+    {
+        public void Apply(OpenApiOperation operation, OperationFilterContext context)
+        {
+            if (operation.Parameters == null)
+                operation.Parameters = new List<OpenApiParameter>();
+
+            operation.Parameters.Add(new OpenApiParameter
+            {
+                Name = "hash",
+                In = ParameterLocation.Header,
+                Required = true
+            });
+        }
+    }
+
+}
