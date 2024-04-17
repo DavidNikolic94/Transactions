228098f4223bf0e65bc1d3468d7fda2e075a142a
David Nikolic <Davidnikolic94@gmail.com>
2024-04-17 16:59:02 +0200

    Add: Folder Services and service classes ITransactionService and TransactionService

---
+++ Services/ITransactionService.cs
@@ -0,0 +1,10 @@
+﻿using Transactions.Models;
+
+namespace Transactions.Services
+{
+    public interface ITransactionService
+    {
+        Task<TransactionResponse> ProcessTransaction(TransactionRequest request, string FromUserId);
+
+    }
+}
---
+++ Services/TransactionService.cs
@@ -0,0 +1,68 @@
+﻿namespace Transactions.Services
+{
+    using System;
+    using Transactions.Data;
+    using Transactions.Models;
+
+    public class TransactionService : ITransactionService
+    {
+        private readonly TransactionDbContext _context;
+
+        public TransactionService(TransactionDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<TransactionResponse> ProcessTransaction(TransactionRequest request, string fromUserId)
+        {
+            // Find the user
+            var rcvUser = await _context.Users.FindAsync(request.UserId);
+            var sndUser = await _context.Users.FindAsync(fromUserId);
+
+            if (sndUser == null)
+            {
+                return new TransactionResponse
+                {
+                    Message = "Sender not found",
+                    Status = 1
+                };
+            }
+
+            if (rcvUser == null)
+            {
+                return new TransactionResponse
+                {
+                    Message = "Receiver not found",
+                    Status = 1
+                };
+            }
+
+            // Check the currency
+            if (sndUser.Currency != request.Currency || rcvUser.Currency != request.Currency)
+            {
+                return new TransactionResponse
+                {
+                    Message = "Invalid currency",
+                    Status = 2
+                };
+            }
+
+            // Update the user's balance
+            sndUser.Balance -= request.Amount;
+            rcvUser.Balance += request.Amount;
+            _context.Users.Update(sndUser);
+            _context.Users.Update(rcvUser);
+            await _context.SaveChangesAsync();
+
+            return new TransactionResponse
+            {
+                TransactionId = Guid.NewGuid().ToString(),
+                Message = "Transaction processed successfully",
+                Status = 0
+            };
+        }
+    }
+
+
+
+}
