ab7cea98c15c46a6e50a0136f1f9a9d0715d4496
David Nikolic <Davidnikolic94@gmail.com>
2024-04-17 20:37:36 +0200

    Mod: TransactionContext added initial entries in the database.

--- Data/TransactionsDbContext.cs
+++ Data/TransactionsDbContext.cs
@@ -17,6 +17,14 @@ namespace Transactions.Data
             modelBuilder.Entity<User>().ToTable("Users");
 
             modelBuilder.Entity<User>().Property(u => u.Balance).HasPrecision(18, 4);
+
+            modelBuilder.Entity<User>().HasData(
+            new User { Id = "1", UserName = "Alice", Currency = "EUR", Balance = 1000.00m },
+            new User { Id = "2", UserName = "Bob", Currency = "USD", Balance = 2000.00m },
+            new User { Id = "3", UserName = "Charlie", Currency = "CAD", Balance = 3000.00m },
+            new User { Id = "4", UserName = "Dave", Currency = "USD", Balance = 4000.00m },
+            new User { Id = "5", UserName = "Eve", Currency = "EUR", Balance = 5000.00m }
+            );
         }
 
     }
