ccf1b670c10624fa783c553a7b6d1b9285bb1e82
David Nikolic <Davidnikolic94@gmail.com>
2024-04-17 20:37:47 +0200

    Add: V2 migrations

---
+++ Migrations/20240417183602_V2.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Transactions.Migrations
+{
+    public partial class V2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Balance", "Currency", "UserName" },
+                values: new object[,]
+                {
+                    { "1", 1000.00m, "EUR", "Alice" },
+                    { "2", 2000.00m, "USD", "Bob" },
+                    { "3", 3000.00m, "CAD", "Charlie" },
+                    { "4", 4000.00m, "USD", "Dave" },
+                    { "5", 5000.00m, "EUR", "Eve" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "1");
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "2");
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "3");
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "4");
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: "5");
+        }
+    }
+}
